998abb449302b9a4d82f78935f142964a562f295
Evan Longwood <24954397+AngryGrizzlyBear@users.noreply.github.com>
2026-04-01 11:16:20 -0700

    Added Javascript functionality.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // The Rusty Nail - Scripts 
+const hamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('nav ul');
+const navLinks = document.querySelectorAll('nav a');
+const currentFilename = window.location.pathname.split('/').pop() || 'index.html';
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle('open')
+    nav.classList.toggle('open')
+    const isExpanded = hamburger.getAttribute('aria-expanded')
+    hamburger.setAttribute('aria-expanded', isExpanded === 'true' ? 'false' : 'true');
+})
+
+navLinks.forEach(link => {
+    const linkedHref = link.getAttribute('href');
+
+    if (linkedHref === currentFilename) {
+        link.classList.add('active');
+    }
+});
